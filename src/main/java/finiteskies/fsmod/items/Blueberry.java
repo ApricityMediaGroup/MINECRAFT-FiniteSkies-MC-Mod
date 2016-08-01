a12c07a368ae65e2462be91f050b16b5a0c85458
Brendan Weirtz <brendanweirtz@gmail.com>
2016-08-01 17:20:03 -0400

    Food now edible

--- src/main/java/finiteskies/fsmod/items/Blueberry.java
+++ src/main/java/finiteskies/fsmod/items/Blueberry.java
@@ -2,9 +2,11 @@ package finiteskies.fsmod.items;
 
 import finiteskies.fsmod.Reference;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemFood;
 
-public class Blueberry extends Item {
+public class Blueberry extends ItemFood {
 	public Blueberry() {
+		super(1, 0.1F, false);
 		setUnlocalizedName(Reference.FiniteItems.BLUEBERRY.getUnlocalizedName());
 		setRegistryName(Reference.FiniteItems.BLUEBERRY.getRegistryName());
 	}
--- src/main/java/finiteskies/fsmod/items/Strawberry.java
+++ src/main/java/finiteskies/fsmod/items/Strawberry.java
@@ -3,9 +3,11 @@ package finiteskies.fsmod.items;
 import finiteskies.fsmod.Reference;
 
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemFood;
 
-public class Strawberry extends Item {
+public class Strawberry extends ItemFood {
 	public Strawberry() {
+		super(1, 0.1F, false);
 		setUnlocalizedName(Reference.FiniteItems.STRAWBERRY.getUnlocalizedName());
 		setRegistryName(Reference.FiniteItems.STRAWBERRY.getRegistryName());
 	}
