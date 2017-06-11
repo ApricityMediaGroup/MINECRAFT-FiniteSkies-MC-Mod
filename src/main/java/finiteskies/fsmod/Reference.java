586fce4a776d939264131fd734b2ae199a3012ff
Brendan Weirtz <brendanweirtz@gmail.com>
2017-06-11 13:10:00 -0400

    prep for 1.12

--- src/main/java/finiteskies/fsmod/Reference.java
+++ src/main/java/finiteskies/fsmod/Reference.java
@@ -6,7 +6,7 @@ public class Reference {
 	public static final String MOD_ID = "fsm";
 	public static final String NAME = "FiniteSkies Mod";
 	public static final String VERSION = "1.1";
-	public static final String ACCEPTED_VERSIONS = "[1.11.2]";
+	public static final String ACCEPTED_VERSIONS = "[1.12]";
 	public static final String CLIENT_PROXY_CLASS = "finiteskies.fsmod.proxy.ClientProxy";
 	public static final String SERVER_PROXY_CLASS = "finiteskies.fsmod.proxy.CommonProxy";
 	public static final String RESOURCE_PREFIX = Reference.MOD_ID + ":";
